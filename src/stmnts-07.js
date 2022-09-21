5e498bdd7916711e0824f68c0d138cf29bfb78d8
Eric Brown <eric.brown8787@gmail.com>
2022-09-21 18:32:33 -0400

    Complete last problem set

--- src/stmnts-07.js
+++ src/stmnts-07.js
@@ -13,7 +13,10 @@
  * ? [JavaScript: The Definitive Guide, Chapter 4.10](https://bit.ly/39lbxnK)
  */
 function logicalAnd(a, b) {
-  // write your code here & return
+  if (a && b) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -23,7 +26,10 @@ function logicalAnd(a, b) {
  * @returns {boolean} - true if a or b is truthy, false if both are falsy
  */
 function logicalOr(a, b) {
-  // write your code here & return
+  if (a || b) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -32,7 +38,7 @@ function logicalOr(a, b) {
  * @returns {boolean} - the opposite of the given boolean
  */
 function invertBoolean(bool) {
-  // write your code here & return
+  return !bool;
 }
 
 /**
@@ -49,7 +55,13 @@ function invertBoolean(bool) {
  * ? [JavaScript: The Definitive Guide, Chapter 5.4.3](https://bit.ly/39hrIlW)
  */
 function numberOfOdds(num) {
-  // write your code here & return
+  let counter = 0;
+  for (let i = 0; i < num; i++) {
+    if (i % 2 !== 0) {
+      counter++;
+    }
+  }
+  return counter;
 }
 
 /**
@@ -62,7 +74,11 @@ function numberOfOdds(num) {
  * ? For example, num is 4 then return 10 because 1 + 2 + 3 + 4 = 10.
  */
 function addUpTheNumbers(num) {
-  // write your code here & return
+  let sum = 0;
+  for (let i = 0; i <= num; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 /**
@@ -81,7 +97,17 @@ function addUpTheNumbers(num) {
  * ?
  */
 function gradeGenerator(score) {
-  // write your code here & return
+  if (score < 60) {
+    return 'F'
+  } else if (score < 70) {
+    return 'D'
+  } else if (score < 80) {
+    return 'C'
+  } else if (score < 90) {
+    return 'B'
+  } else {
+    return 'A'
+  }
 }
 
 /**
@@ -97,11 +123,16 @@ function gradeGenerator(score) {
  * ? return a string written like:
  * ? Francine got an A
  * ? David got a B
- * ? note: you have to use English grammar's correct indefinite article 
+ * ? note: you have to use English grammar's correct indefinite article
  * ? it's 'an A' (not a A) and 'an F' (not a F)
  */
 function getGrade(name, score) {
-  // write your code here & return
+  const letterGrade = gradeGenerator(score);
+  if (letterGrade === 'A' || letterGrade === "F") {
+    return `${name} got an ${letterGrade}`;
+  } else {
+    return `${name} got a ${letterGrade}`;
+  }
 }
 
 module.exports = {
